a7ab5155824b7c6aa0543c95e3bb949ec6a88edc
bRANC <branc9.kb@gmail.com>
2015-09-22 21:31:49 +0200

    első tyommit
    ez már egy futó program

---
+++ app/src/main/java/com/funyirok/branc/elso_program/MainActivity.java
@@ -0,0 +1,61 @@
+package com.funyirok.branc.elso_program;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public int a = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                a++;
+                TextView tv1 = (TextView) findViewById(R.id.textView);
+                tv1.setText("Punished: " + a + " times!");
+                Toast.makeText(MainActivity.this, "AWW Yiss!!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
